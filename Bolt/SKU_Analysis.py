d7428bd77787a033111eab95d1968ed2374e4b56
Mavericky007 <144117566+Mavericky007@users.noreply.github.com>
2024-01-13 00:29:29 +0530

    Update SKU_Analysis.py

--- Bolt/SKU_Analysis.py
+++ Bolt/SKU_Analysis.py
@@ -32,7 +32,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    os.chdir(r"Bolt/data")
+    # os.chdir(r"Bolt/data")
     # df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
     df = pd.read_excel("data1.xlsx")
 
