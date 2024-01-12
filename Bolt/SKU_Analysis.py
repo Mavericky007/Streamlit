89f01887be32fa2fe132f96c3a2ff2e7cff95171
Mavericky007 <144117566+Mavericky007@users.noreply.github.com>
2024-01-13 00:55:08 +0530

    Update SKU_Analysis.py

--- Bolt/SKU_Analysis.py
+++ Bolt/SKU_Analysis.py
@@ -15,11 +15,11 @@ warnings.filterwarnings('ignore') # helps ignore all warnings in the Dashboard
 st.set_page_config(page_title="Bolt SDA Assessment!!!", page_icon="/Users/shaikh.jounaid/Desktop/flash.png",layout="wide")
 
 # Inserting an image into the title
-image = Image.open("utilities/Bolt_Logo.png")
+# image = Image.open("utilities/Bolt_Logo.png")
 
 t1, t2 = st.columns([1, 2])
-with t1:
-    st.image(image, use_column_width=False, width=150)
+# with t1:
+#     st.image(image, use_column_width=False, width=150)
 with t2:
     st.title("Grocery Stores SKUs Analysis")
     st.markdown('<style>div.block-container{padding-top:3rem;}</style>',unsafe_allow_html=True)
