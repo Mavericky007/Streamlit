25059ef2a8cdf76cb9d8e5215e8558b743d0d0cb
Mavericky007 <144117566+Mavericky007@users.noreply.github.com>
2024-01-13 00:36:13 +0530

    Update SKU_Analysis.py

--- Bolt/SKU_Analysis.py
+++ Bolt/SKU_Analysis.py
@@ -5,6 +5,7 @@
 
 import streamlit as st
 import plotly.express as px
+from PIL import Image
 import pandas as pd
 import os
 import warnings
@@ -13,12 +14,12 @@ warnings.filterwarnings('ignore') # helps ignore all warnings in the Dashboard
 
 st.set_page_config(page_title="Bolt SDA Assessment!!!", page_icon="/Users/shaikh.jounaid/Desktop/flash.png",layout="wide")
 
-# # Inserting an image into the title
-# image = open("Bolt-Logo.png", 'rb')
+# Inserting an image into the title
+image = Image.open("Bolt-Logo.png")
 
 t1, t2 = st.columns([1, 2])
-# with t1:
-#     st.image("Bolt_Logo.png", use_column_width=False, width=150)
+with t1:
+    st.image(image, use_column_width=False, width=150)
 with t2:
     st.title("Grocery Stores SKUs Analysis")
     st.markdown('<style>div.block-container{padding-top:3rem;}</style>',unsafe_allow_html=True)
